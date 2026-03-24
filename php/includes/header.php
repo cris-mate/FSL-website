356b48338798208f6a6fad33dda3ac7342f62554
Cristian <46843669+cris-mate@users.noreply.github.com>
2026-03-24 20:16:49 +0000

    Add HTML structure and navigation to header.php

--- php/includes/header.php
+++ php/includes/header.php
@@ -1,1 +1,25 @@
-
+<!-- php/includes/header.php -->
+<!DOCTYPE html>
+<html lang="it">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Matei Manuela Natalis FSL</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/MateiManuelaNatalis_FSL/css/style.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div class="container">
+    <a class="navbar-brand" href="/MateiManuelaNatalis_FSL/index.php">Matei Manuela Natalis FSL</a>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item"><a class="nav-link" href="/MateiManuelaNatalis_FSL/php/azienda.php">Azienda</a></li>
+        <li class="nav-item"><a class="nav-link" href="/MateiManuelaNatalis_FSL/php/stage.php">Stage</a></li>
+        <li class="nav-item"><a class="nav-link" href="/MateiManuelaNatalis_FSL/php/orientamento.php">Orientamento</a></li>
+        <li class="nav-item"><a class="nav-link" href="/MateiManuelaNatalis_FSL/php/gestione.php">Gestione DB</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<main class="container my-4">
