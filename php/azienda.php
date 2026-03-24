7a621028f9cd6c5b34266c22e7a46f9138f9af5d
Cristian <46843669+cris-mate@users.noreply.github.com>
2026-03-24 21:20:11 +0000

    Refactor company presentation section in azienda.php

--- php/azienda.php
+++ php/azienda.php
@@ -1,30 +1,50 @@
 <?php include '../php/includes/header.php'; ?>
 
-<div class="container mt-5">
+<h2 class="section-title mb-4">Presentazione dell'Azienda</h2>
 
-            <h2>Presentazione dell'Azienda</h2>
+<!-- Info Azienda -->
+<div class="card card-hover mb-4">
+  <div class="card-body">
+    <h4 class="card-title mb-3">🏢 Studio Commercialista</h4>
+    <p>
+      Nel 2023 ho svolto il mio percorso PCTO presso uno studio di commercialista
+      situato a Montichiari. Lo studio si occupa principalmente di consulenza fiscale,
+      contabile e amministrativa per imprese e privati.
+    </p>
+  </div>
+</div>
 
-            <h4>Studio Commercialista</h4>
+<!-- Servizi e Clienti -->
+<div class="row g-4 mb-4">
+  <div class="col-md-6">
+    <div class="card h-100 card-hover">
+      <div class="card-body">
+        <h5 class="card-title">Servizi offerti</h5>
+        <p>
+          Lo studio fornisce servizi di gestione della contabilità ordinaria e semplificata,
+          elaborazione dichiarazioni dei redditi, consulenza fiscale, gestione buste paga
+          e assistenza alle imprese nelle pratiche amministrative.
+        </p>
+      </div>
+    </div>
+  </div>
+  <div class="col-md-6">
+    <div class="card h-100 card-hover">
+      <div class="card-body">
+        <h5 class="card-title">Tipologia di clienti</h5>
+        <p>
+          I clienti principali sono piccole e medie imprese, liberi professionisti
+          e privati cittadini.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
 
-            <p>
-                Nel 2023 ho svolto il mio percorso PCTO presso uno studio di commercialista 
-                situato a Montichiari. Lo studio si occupa principalmente di consulenza fiscale,
-                contabile e amministrativa per imprese e privati.
-            </p>
-
-
-            <h5>Servizi offerti</h5>
-            <p>
-                Lo studio fornisce servizi di gestione della contabilità ordinaria e semplificata,
-                elaborazione dichiarazioni dei redditi, consulenza fiscale, gestione buste paga
-                e assistenza alle imprese nelle pratiche amministrative.
-            </p>
-
-            <h5>Tipologia di clienti</h5>
-            <p>
-                I clienti principali sono piccole e medie imprese, liberi professionisti e privati cittadini.
-            </p>
-
-        </div>
+<!-- Navigazione tra le sezioni -->
+<div class="d-flex justify-content-between mt-5">
+  <a href="/MateiManuelaNatalis_FSL/index.php" class="btn btn-outline-dark">&larr; Home</a>
+  <a href="/MateiManuelaNatalis_FSL/php/stage.php" class="btn btn-outline-dark">Stage &rarr;</a>
+</div>
 
 <?php include '../php/includes/footer.php'; ?>
