b9808a00c2da830d96c585eac78d29e44e822cc0
Cristian <46843669+cris-mate@users.noreply.github.com>
2026-03-24 21:07:30 +0000

    Update azienda.php with company details
    Added company presentation and services offered.

--- php/azienda.php
+++ php/azienda.php
@@ -1,5 +1,30 @@
 <?php include '../php/includes/header.php'; ?>
 
-<!-- content -->
+<div class="container mt-5">
+
+            <h2>Presentazione dell'Azienda</h2>
+
+            <h4>Studio Commercialista</h4>
+
+            <p>
+                Nel 2023 ho svolto il mio percorso PCTO presso uno studio di commercialista 
+                situato a Montichiari. Lo studio si occupa principalmente di consulenza fiscale,
+                contabile e amministrativa per imprese e privati.
+            </p>
+
+
+            <h5>Servizi offerti</h5>
+            <p>
+                Lo studio fornisce servizi di gestione della contabilità ordinaria e semplificata,
+                elaborazione dichiarazioni dei redditi, consulenza fiscale, gestione buste paga
+                e assistenza alle imprese nelle pratiche amministrative.
+            </p>
+
+            <h5>Tipologia di clienti</h5>
+            <p>
+                I clienti principali sono piccole e medie imprese, liberi professionisti e privati cittadini.
+            </p>
+
+        </div>
 
 <?php include '../php/includes/footer.php'; ?>
