b1776aee615c1b9822832d8999e4d93c23b419dd
Cristian <46843669+cris-mate@users.noreply.github.com>
2026-03-24 20:19:45 +0000

    Create database and stage table in database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -1,1 +1,11 @@
+CREATE DATABASE pcto_db; 
+USE pcto_db; 
 
+CREATE TABLE stage ( 
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    azienda VARCHAR(100) NOT NULL, 
+    ruolo VARCHAR(100) NOT NULL, 
+    data_inizio DATE, 
+    data_fine DATE, 
+    descrizione TEXT 
+);
